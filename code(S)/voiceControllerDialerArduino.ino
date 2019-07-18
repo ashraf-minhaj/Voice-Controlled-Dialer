1c6b78c57093e11f0d33b78c8ce2825cf6f87e50
Ashrafur Rahman Minhaj <ashraf_minhaj@yahoo.com>
2019-07-19 01:22:31 +0600

    Add files via upload

---
+++ code(S)/voiceControllerDialerArduino.ino
@@ -0,0 +1,36 @@
+/*  Voice Controlled Call dialer ***
+ *  by: Ashraf Minhaj
+ *  mail: ashraf_minhaj@yahoo.com
+ *  blogpost/ Tutorial: https://ashrafminhajgb.blogspot.com
+*/
+
+/*
+ * Required:
+ * voiceControlledDialer.py script running on raspberry pi or pc
+ */
+
+ String x;
+
+ void setup() // put your setup code here, to run once
+{
+  Serial.begin(9600);
+  delay(1000);
+  //a bit delay to make let the sim808 connect online
+}
+
+void loop()
+{
+  while(Serial.available() > 0)
+  {
+    x = Serial.readStringUntil("\n");
+
+    String toSend = "ATD"; //make call "ATDNUMBER"
+    toSend += x;
+
+    Serial.println(toSend);
+  }
+
+  x = "";
+  
+}
+
